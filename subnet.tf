34361aabf4df1bf7650228249bdd68fde1ef5050
aman46788 <amanchauhan1182@gmail.com>
2025-09-28 22:51:08 +0530

    updated subnet

--- subnet.tf
+++ subnet.tf
@@ -5,3 +5,10 @@ resource "azurerm_subnet" "test-subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/25"]
 }
+resource "azurerm_subnet" "durgesh-subnet" {
+  depends_on = [ azurerm_virtual_network.vnet ]
+  name                 = "shwetank-subnet"
+  resource_group_name  = azurerm_resource_group.test-rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/32"]
+}
